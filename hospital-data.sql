11a664a8b7c0ed82472ffa5b857c80e6bdeae31a
Tala Shraim <128409477+talaS118@users.noreply.github.com>
2026-01-21 20:17:42 -0600

    Update hospital-data.sql
    employee and patient data fix

--- hospital-data.sql
+++ hospital-data.sql
@@ -10,25 +10,24 @@ INSERT INTO Room (roomID, capacity, feePerNight) VALUES
 
 
 INSERT INTO Employee (employeeID, name, certificationNumber, street, city, state, phoneNumber) VALUES
-(67, 'Tala Shraim', 6712, '3219 S 79th ave', 'Justice', 'IL', '708-647-6890'),
-(68, 'Aseel Suleiman', 6713, '9322 S 39th ave', 'Bridgeview', 'IL', '708-231-4721'),
-(69, 'Laya Nasan', 6714, '9123 S 89th ave', 'Oaklawn', 'IL', '708-924-9012'),
-(70, 'Ameer Mohamed', 6715, '3331 S 19th ave', 'Naperville', 'IL', '708-305-3465'),
-(71, 'Sarah Shraim', 6716, '9221 S 54th ave', 'Worth', 'IL', '708-762-5674'),
-(72, 'Nadia Khatib', 6717, '4536 S 29th ave', 'Palos Heights', 'IL', '708-839-8761'),
-(73, 'Kay Smith', 6718, '2111 S 89th ave', 'Palos Park', 'IL', '708-450-2394'),
-(74, 'Sawsan Yousef', 6719, '3519 S 74th ave', 'Orland Park', 'IL', '708-596-7820'),
-(75, 'Zach Naeim', 6720, '3281 S 49th ave', 'Tinley Park', 'IL', '708-421-1289'),
-(76, 'Ahmad Yousef', 6721, '9321 S 59th ave', 'Chicago Ridge', 'IL', '708-357-4519');
+(67, 'Employee_067', 6712, '3219 S 79th ave', 'Worth', 'IL', '708-647-6890'),
+(68, 'Employee_068', 6713, '9322 S 39th ave', 'Bridgeview', 'IL', '708-231-4721'),
+(69, 'Employee_069', 6714, '9123 S 89th ave', 'Oaklawn', 'IL', '708-924-9012'),
+(70, 'Employee_070', 6715, '3331 S 19th ave', 'Naperville', 'IL', '708-305-3465'),
+(71, 'Employee_071', 6716, '9221 S 54th ave', 'Worth', 'IL', '708-762-5674'),
+(72, 'Employee_072', 6717, '4536 S 29th ave', 'Palos Heights', 'IL', '708-839-8761'),
+(73, 'Employee_073', 6718, '2111 S 89th ave', 'Palos Park', 'IL', '708-450-2394'),
+(74, 'Employee_074', 6719, '3519 S 74th ave', 'Orland Park', 'IL', '708-596-7820'),
+(75, 'Employee_075', 6720, '3281 S 49th ave', 'Tinley Park', 'IL', '708-421-1289'),
+(76, 'Employee_076', 6721, '9321 S 59th ave', 'Chicago Ridge', 'IL', '708-357-4519');
 
 
 INSERT INTO Patient (patientID, name, address, phoneNumber) VALUES
-(100, 'Raneem Bader', '3203 S 59th Ave, Oak Lawn, IL', '708-321-9876'),
-(101, 'Anas Ramadan', '4204 S 49th Ave, Palos Heights, IL', '708-432-6543'),
-(102, 'Lynn Idris', '5105 S 39th Ave, Tinley Park, IL', '708-543-2345'),
-(103, 'Deema Nahleh', '6066 S 29th Ave, Orland Park, IL', '708-654-6789'),
-(104, 'Mark Nowak', '7207 S 19th Ave, Naperville, IL', '708-765-3456');
-
+(100, 'Patient_100', '3203 S 59th Ave, Oak Lawn, IL', '708-321-9876'),
+(101, 'Patient_101', '4204 S 49th Ave, Palos Heights, IL', '708-432-6543'),
+(102, 'Patient_102', '5105 S 39th Ave, Tinley Park, IL', '708-543-2345'),
+(103, 'Patient_103', '6066 S 29th Ave, Orland Park, IL', '708-654-6789'),
+(104, 'Patient_104', '7207 S 19th Ave, Naperville, IL', '708-765-3456');
 
 INSERT INTO Physician (employeeID, fieldOfExpertise) VALUES
 (67, 'Cardiology'),
